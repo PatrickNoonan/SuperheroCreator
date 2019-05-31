ed6d109a7a473465dbafce0802541ad44b4bea9c
Patrick <Patricknnoonan@gmail.com>
2019-05-31 10:24:20 -0500

    Halfway point following set up video

---
+++ SuperheroCreator/Models/Hero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperheroCreator.Models
+{
+    public class Hero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string power { get; set; }
+    }
+}
--- SuperheroCreator/Models/IdentityModels.cs
+++ SuperheroCreator/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -20,6 +21,7 @@ namespace SuperheroCreator.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Hero> Heroes { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
